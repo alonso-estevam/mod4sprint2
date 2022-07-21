f1f11621db9414b10b8bf82089ffc0914f69e5b5
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 18:30:20 -0300

    feat: add route for post the tips

--- src/controllers/tipsController.js
+++ src/controllers/tipsController.js
@@ -6,6 +6,14 @@ const tipsController = (app) => {
         res.status(200).json({"random tip": randomTip.showTip()});
     })
 
+    app.post('/add-tip', (req, res) => {
+        const body = req.body;
+        const tip = new Tips(body.text);
+        tip.addTip()
+        res.json({"tip": tip})
+        res.send("Tip successfully added!")
+    })
+
 }
 
 export default tipsController
