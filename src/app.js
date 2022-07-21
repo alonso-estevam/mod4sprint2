eeb9329c71ae724b976fe2eab49085b525ff24f8
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 19:10:13 -0300

    feat: import controllers

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import tipsDb from './database/tipsDb.js'
+import tipsController from '../src/controllers/tipsController.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json(tipsDb);
-})
+tipsController(app);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
