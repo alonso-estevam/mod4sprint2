bf4b45df927dd41ef4f3564778bb4ea68409042b
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 22:38:07 -0300

    fix: resolve err http headers sent

--- src/controllers/tipsController.js
+++ src/controllers/tipsController.js
@@ -3,15 +3,17 @@ import Tips from '../models/tipsModel.js';
 const tipsController = (app) => {
     app.get('/tips', (req, res) => {
         const randomTip = new Tips();
-        res.status(200).json({"random tip": randomTip.showTip()});
+        res.status(200).json({"tip": randomTip.showTip()});
     })
 
     app.post('/add-tip', (req, res) => {
         const body = req.body;
-        const tip = new Tips(body.text);
-        tip.addTip()
-        res.json({"tip": tip})
-        res.send("Tip successfully added!")
+        const tip = new Tips(body);
+        tip.addTip();
+        res.json({
+            ...tip, 
+            "message":"Tip successfully added!"
+        });
     })
 
 }
